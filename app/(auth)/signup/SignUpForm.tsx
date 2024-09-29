6ecb9f5546afb9c5e2c3404e4f834ba1ae823d26
2div <mhersi819@gmail.com>
2024-09-29 16:36:53 +0300

    Input form added

--- app/(auth)/signup/SignUpForm.tsx
+++ app/(auth)/signup/SignUpForm.tsx
@@ -1,10 +1,19 @@
 "use client"
 
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 import { signUpSchema, signUpValues } from '@/lib/validation'
 import {zodResolver} from '@hookform/resolvers/zod'
+import { useState, useTransition } from 'react'
 import { useForm } from 'react-hook-form'
+import { signUp } from './actions'
 
 function SignUpForm() {
+
+    const [error, setError] = useState<string>();
+    const [isPending, startTransition] = useTransition();
+
     const form = useForm<signUpValues>({
         resolver: zodResolver(signUpSchema),
         defaultValues: {
@@ -15,11 +24,63 @@ function SignUpForm() {
     })
 
     async function onSubmit(values: signUpValues) {
-        //
+        setError(undefined);
+        startTransition(async () => {
+          const {error} = await signUp(values);
+          if (error) setError(error)
+
+        })
     }
 
   return (
-    <div>SignUpForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
+        { error && <p className='text-center text-destructive'>{error}</p>}
+        <FormField
+          control={form.control}
+          name='username'
+          render={({field}) => (
+            <FormItem >
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({field}) => (
+            <FormItem >
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" type='email' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='password'
+          render={({field}) => (
+            <FormItem >
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Password" type='password' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'className='w-full'>
+          Create account
+        </Button>
+      </form>
+      
+    </Form>
   )
 }
 
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,42 +1,41 @@
-import { Metadata } from 'next'
-import React from 'react'
-import signUpImage from "@/assets/signup-image.jpg"
-import Image from 'next/image'
-import Link from 'next/link'
-import SignUpForm from './SignUpForm'
+import { Metadata } from "next";
+import React from "react";
+import signUpImage from "@/assets/signup-image.jpg";
+import Image from "next/image";
+import Link from "next/link";
+import SignUpForm from "./SignUpForm";
 
 export const metadata: Metadata = {
-    title: " Sign Up"
-}
+  title: " Sign Up",
+};
 
 function page() {
   return (
-    <main className='flex h-screen items-center justify-center p-5'>    
-        <div className="shadow-2xl flex h-full max-h-[40rem] w-full max-w-[64rem] rounded-2xl overflow-hidden bg-card">
-                <div className="md:w-1/2 w-full space-y-10 overflow-y-auto p-10">
-                    <div className="space-y-1 text-center">
-                        <h1 className="text-3xl font-bold">
-                            Sign up to bugbook
-                        </h1>
-                        <p className="text-muted-foreground">
-                            A place where even <span className='italic'>you</span> can find a friend
-                        </p>
-                    </div>
-                    <div className="space-y-5">
-                        <SignUpForm />
-                        <Link href="login" className="block text-center hover:underline">
-                            Already have an account? Log in
-                        </Link>
-                    </div>
-                </div>
-            <Image
-                src={signUpImage}
-                alt=''
-                className='w-1/2 hidden md:block object-cover'
-            />
+    <main className="flex h-screen items-center justify-center p-5">
+      <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
+        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+          <div className="space-y-1 text-center">
+            <h1 className="text-3xl font-bold">Sign up to bugbook</h1>
+            <p className="text-muted-foreground">
+              A place where even <span className="italic">you</span> can find a
+              friend
+            </p>
+          </div>
+          <div className="space-y-5">
+            <SignUpForm />
+            <Link href="/login" className="block text-center hover:underline">
+              Already have an account? Log in
+            </Link>
+          </div>
         </div>
+        <Image
+          src={signUpImage}
+          alt=""
+          className="hidden w-1/2 object-cover md:block"
+        />
+      </div>
     </main>
-  )
+  );
 }
 
-export default page
+export default page;
